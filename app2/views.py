1b47fc7604767dc5487c09bc5fd24b426d0bf3ab
sreej683 <sreej683@gmail.com>
2024-11-18 15:37:17 +0530

    first commit

--- app2/views.py
+++ app2/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 
-
 # Create your views here.
-
 from django.http import HttpResponse
 
 def lasya(request):
-    return HttpResponse("<h1><marquee>lasya is a good girl</marquee></h1>")
-
-
- 
+    return HttpResponse('<h1><marquee>lasya is good girl</marquee></h1>')
--- app3/views.py
+++ app3/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 from django.http import HttpResponse
-def tamil(request):
-    return HttpResponse("<h1><marquee>Tamil is easy to learn</marquee></h1>")
+
+def yash(request):
+    return HttpResponse('<h1><marquee>yash is handsome</marquee></h1>')
